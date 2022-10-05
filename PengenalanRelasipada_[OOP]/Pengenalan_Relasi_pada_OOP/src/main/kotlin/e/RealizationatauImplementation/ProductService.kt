64653715da8f35317ffc4a057f917d3f5c689ff2
AkhmadAldi19 <aldiakhmad15@gmail.com>
2022-10-05 11:52:37 +0700

    Realization atau Implementation

---
+++ PengenalanRelasipada_[OOP]/Pengenalan_Relasi_pada_OOP/src/main/kotlin/e/RealizationatauImplementation/ProductService.kt
@@ -0,0 +1,15 @@
+package e.RealizationatauImplementation
+
+interface ProductService {
+    fun getProduct(id: String)
+    fun calculateProductPrice()
+}
+
+class ProductServiceImpl() : ProductService{
+    override fun getProduct(id: String) {
+        // Implementation code
+    }
+    override fun calculateProductPrice() {
+        // Implementation code
+    }
+}
